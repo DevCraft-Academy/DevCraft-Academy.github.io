ccc8adeb2a9ad7c10de729a7a1cc7bb63fee127e
Clemens Helm <clemens.helm@gmail.com>
2023-06-30 12:50:09 +0200

    Spice up deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+echo "=== Building Jekyll site ===\n"
 jekyll build
+echo "\n=== Syncing to S3 ===\n"
 aws s3 sync _site s3://devcraft.academy --size-only --storage-class REDUCED_REDUNDANCY
-aws cloudfront create-invalidation --distribution-id E1KPT6E6WB23KN --paths "/*"
+echo "\n=== Invalidating CloudFront cache ===\n"
+aws cloudfront create-invalidation --distribution-id E1KPT6E6WB23KN --paths "/*" --output text | cat
+echo "\n=== Successfully deployed devcraft.academy ==="
