611b8de0d3ee141d638740cdd5e42d61d95ea363
Clemens Helm <clemens.helm@gmail.com>
2023-09-14 13:02:26 +0200

    Add network tab demo

---
+++ uebungen/network-tab-demo/assets/script.js
@@ -0,0 +1,18 @@
+// Create a large JavaScript file (for demonstration purposes)
+const largeArray = new Array(1000000).fill(
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+);
+
+fetch("https://catfact.ninja/fact");
+
+// Simulate a long-running operation
+function simulateLoad() {
+  console.log("Start loading large.js");
+  for (let i = 0; i < largeArray.length; i++) {
+    // Simulate some processing
+  }
+  console.log("Finished loading large.js");
+}
+
+// Call the simulation function when the page loads
+window.addEventListener("load", simulateLoad);
