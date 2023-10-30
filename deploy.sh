ba09623ff19a8ca37a3992217c2e69afb36e2f83
Clemens Helm <clemens.helm@gmail.com>
2023-10-30 09:57:30 +0100

    Fix bundler problem in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "=== Building Jekyll site ===\n"
-jekyll build
+bundle exec jekyll build
 echo "\n=== Syncing to S3 ===\n"
 aws s3 sync _site s3://devcraft.academy --size-only --storage-class REDUCED_REDUNDANCY
 echo "\n=== Invalidating CloudFront cache ===\n"
