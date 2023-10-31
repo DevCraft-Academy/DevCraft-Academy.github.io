b00e9d701b14c896448a843d48f48d2f9d00d319
Edda Bueltemeyer <eddagerlinde.bueltemeyer@gmail.com>
2023-10-31 10:53:57 +0100

    fix: change employee image path and css corrected

--- modify_team_data.rb
+++
@@ -1,9 +0,0 @@
-require 'yaml'
-
-employee_data = YAML.load_file('C:\dev_projekte_git\Helmcraft_GmbH\devcraft_landingpage\landingpage\_data\teammembers.yml')
-
-employee_data.each do |employee|
-  employee['image'] = "/assets/images/#{File.basename(employee['image'])}"
-end
-
-File.open('C:\dev_projekte_git\Helmcraft_GmbH\devcraft_landingpage\landingpage\_data\teammembers.yml', 'w') { |file| file.write(YAML.dump(employee_data))}
