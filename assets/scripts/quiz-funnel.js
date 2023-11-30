734f515c01f0e2b011eda2e7613ee491474ae621
Edda Bueltemeyer <eddagerlinde.bueltemeyer@gmail.com>
2023-11-30 20:43:31 +0100

    fix: Funnel functions and reply messages edited

--- assets/scripts/quiz-funnel.js
+++ assets/scripts/quiz-funnel.js
@@ -51,22 +51,42 @@ function checkAnswers() {
 }
 
 function showResults() {
-    const resultText = document.getElementById("resultText");
-    const resultMessage = document.getElementById("resultMessage");
-    const scheduleCTA = document.getElementById("scheduleCTA");
+    const correctQuiz1 = document.getElementById("correctQuiz1");
+    const correctQuiz2 = document.getElementById("correctQuiz2");
+    const falseQuiz = document.getElementById("falseQuiz");
+
+    // Show appropriate answers based on user responses
+    const quiz1Answers = userAnswers["quiz-1"];
+    const quiz2Answers = userAnswers["quiz-2"];
+    const quiz4Answers = userAnswers["quiz-4"];
+    const quiz5Answers = userAnswers["quiz-5"];
 
     // Compare results
     const correctCount = checkAnswers();
 
-    // Decide which text and button to display based on the results
-    if (correctCount === Object.keys(userAnswers).length) {
-        resultMessage.textContent = "Herzlichen Glückwunsch, Du erfüllst unsere Anforderungen für eine Teilnahme!";
-        scheduleCTA.style.display = "block";
-    } else {
-        resultMessage.textContent = "Tut uns leid, leider erfüllst du unsere Anforderungen für eine Teilnahme nicht! Wir wünschen Dir viel Erfolg für Deinen weiteren beruflichen Weg.";
+   
+    if (quiz1Answers && !correctAnswers["quiz-1"].some(answer => quiz1Answers.includes(answer))) {
+        quiz1Message.textContent = "Grundsätzlich brauchst Du mindestens 6 Monate Berufserfahrung.";
     }
 
-    // Show the result
-    resultText.style.display = "block";
+    if (quiz2Answers && !correctAnswers["quiz-2"].includes(quiz2Answers[0])) {
+        quiz2Message.textContent = "Die Weiterbildung bezieht sich inhaltlich auf Full Stack Web Development. Wie es aussieht kommst du aus einem anderen Bereich, bist du sicher, dass du dich im Web Development weiterbilden möchtest?";
+    }
+
+    if (quiz4Answers && !correctAnswers["quiz-4"].includes(quiz4Answers[0])) {
+        quiz4Message.textContent = "Für einen erfolgreichen Abschluss, musst du bereit sein zumindest 1 Stunde täglich für die Weiterbildung aufzuwenden. Es ist nicht vorgesehen den Lehrgang in geblockten Einheiten durchzuführen.";
+    }
+
+    if (quiz5Answers && !correctAnswers["quiz-5"].includes(quiz5Answers[0])) {
+        quiz5Message.textContent = "Die sorgfältig geplante Weiterbildung strebt die Zugehörigkeit zu führenden Bildungsinstituten an und soll unseren Absolventen als Referenz dienen. Niedrigere Preise würden unsere Werte von Aktualität und Fachkompetenz gefährden.";
+    }
+
+    // Decide which text and button to display based on the results
+    if (correctCount === Object.keys(userAnswers).length) {
+        correctQuiz1.textContent = "Herzlichen Glückwunsch, Du erfüllst unsere Anforderungen für eine Teilnahme!";
+        correctQuiz2.textContent = "Nutze ein kostenloses Erstgespräch, um deine Karriere im Web Developmet zu beleuchten.";
+    } else {
+        falseQuiz.textContent = "Wenn du denkst, dass du trotzdem an unserem Programm teilzunehmen kannst, dann vereinbare ein Gespräch mit uns, und wir klären das ab.";
+    } 
 }
 
