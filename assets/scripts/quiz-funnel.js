dfb87307afebc6db9e09ddabbb04eeb3efa255f5
Edda Bueltemeyer <eddagerlinde.bueltemeyer@gmail.com>
2023-12-06 11:59:30 +0100

    fix: Button surface clickable and feedback output adjusted

--- assets/scripts/quiz-funnel.js
+++ assets/scripts/quiz-funnel.js
@@ -94,15 +94,20 @@ function showResults() {
         }
     }
 
+    const totalQuestions = Object.keys(correctAnswers).length;
+
     // Compare results
     const correctCount = checkAnswers();
 
     // Decide which text and button to display based on the results
-    if (correctCount === Object.keys(userAnswers).length) {
+    if (correctCount === totalQuestions) {
         correctQuiz1.textContent = "Herzlichen Glückwunsch, Du erfüllst unsere Anforderungen für eine Teilnahme!";
         correctQuiz2.textContent = "Nutze ein kostenloses Erstgespräch, um deine Karriere im Web Development zu beleuchten.";
+        falseQuiz.textContent = "";
     } else {
         falseQuiz.textContent = "Wenn du denkst, dass du trotzdem an unserem Programm teilzunehmen kannst, dann vereinbare ein Gespräch mit uns, und wir klären das ab.";
+        correctQuiz1.textContent = "";
+        correctQuiz2.textContent = "";
     } 
 }
 
